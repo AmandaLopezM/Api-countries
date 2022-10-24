8f3f5460eac96c4071a7f8a54ba8ca6660f042e2
Amanda Lopez <amandapaolalopezm@gmail.com>
2022-10-24 01:32:37 -0500

    Añadir estructura html

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+ const [paises, setPaises] = React.useState([])
+ console.log(paises)
+  React.useEffect(() => {
+    obtenerDatos()
+  }, [])
+  const obtenerDatos = async() => {
+    const data = await fetch('https://restcountries.com/v3.1/all')
+    const countries = await data.json()
+    setPaises(countries)
+  }
+  return(
+    <div>
+      <h1>Paises</h1>
+      
+      <ul>
+        {paises.map((pais) => {
+          return <li>
+            <img src={pais.flags.png} alt=""></img>
+            <h2>{pais.name.common}</h2>
+            <ul>
+              <li><span className='negrita'>Population:</span> {pais.population}</li>
+              <li><span className='negrita'>Region:</span> {pais.region}</li>
+              <li><span className='negrita'>Capital:</span> {pais.capital}</li>
+            </ul>
+          </li>
+        })}
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
